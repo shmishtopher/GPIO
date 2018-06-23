a51a63626f6726eddbfd846cfeaf9fca9b0e4d5a
shmishtopher <shmish90@gmail.com>
2018-06-23 16:03:04 -0400

    defined bitflags

--- src/ar_gpio.rs
+++ src/ar_gpio.rs
@@ -8,3 +8,27 @@ struct gpiochip_info {
   label: [libc::c_char; 32],
   lines: libc::uint32_t
 }
+
+#[repr(C)]
+struct gpioline_info {
+  line_offset: libc::uint32_t,
+  flags: libc::uint32_t,
+  name: [libc::c_char; 32],
+  consumer: [libc::c_char; 32]
+}
+
+mod RequestFlags {
+  const INPUT: libc::uint32_t =       0x01 << 0;
+  const OUTPUT: libc::uint32_t =      0x01 << 1;
+  const ACTIVE_LOW: libc::uint32_t =  0x01 << 2;
+  const OPEN_DRAIN: libc::uint32_t =  0x01 << 3;
+  const OPEN_SOURCE: libc::uint32_t = 0x01 << 4;
+}
+
+mod LineFlags {
+  const KERNEL: libc::uint32_t =      0x01 << 0;
+  const IS_OUT: libc::uint32_t =      0x01 << 1;
+  const ACTIVE_LOW: libc::uint32_t =  0x01 << 2;
+  const OPEN_DRAIN: libc::uint32_t =  0x01 << 3;
+  const OPEN_SOURCE: libc::uint32_t = 0x01 << 4;
+}
