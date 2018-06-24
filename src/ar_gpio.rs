1484d3572b42065e872131fb3020ddc8745867c9
Shmish <shmish90@gmail.com>
2018-06-24 18:39:08 -0400

    fixed build errors

--- src/ar_gpio.rs
+++ src/ar_gpio.rs
@@ -11,11 +11,10 @@ extern crate nix;
 extern crate libc;
 
 use std::os::unix::io::RawFd;
-use std::os::unix::io::IntoRawFd;
 use std::os::unix::io::FromRawFd;
 use std::os::unix::io::AsRawFd;
 
-mod RequestFlags {
+mod request_flags {
   const INPUT: u32 =       0x01 << 0;
   const OUTPUT: u32 =      0x01 << 1;
   const ACTIVE_LOW: u32 =  0x01 << 2;
@@ -23,7 +22,7 @@ mod RequestFlags {
   const OPEN_SOURCE: u32 = 0x01 << 4;
 }
 
-mod LineFlags {
+mod line_flags {
   const KERNEL: u32 =      0x01 << 0;
   const IS_OUT: u32 =      0x01 << 1;
   const ACTIVE_LOW: u32 =  0x01 << 2;
@@ -32,14 +31,14 @@ mod LineFlags {
 }
 
 #[repr(C)]
-struct gpiochip_info {
+pub struct gpiochip_info {
   name: [libc::c_char; 32],
   label: [libc::c_char; 32],
   lines: libc::uint32_t,
 }
 
 #[repr(C)]
-struct gpioline_info {
+pub struct gpioline_info {
   line_offset: libc::uint32_t,
   flags: libc::uint32_t,
   name: [libc::c_char; 32],
@@ -47,7 +46,7 @@ struct gpioline_info {
 }
 
 #[repr(C)]
-struct gpiohandle_request {
+pub struct gpiohandle_request {
   lineoffsets: [libc::uint32_t; 64],
   flags: libc::uint32_t,
   default_values: [libc::uint8_t; 64],
@@ -57,17 +56,17 @@ struct gpiohandle_request {
 }
 
 #[repr(C)]
-struct gpiohandle_data {
+pub struct gpiohandle_data {
   values: [libc::uint8_t; 64],
 }
 
-ioctl!(read get_chipinfo with 0xB4, 0x01; gpiochip_info);
-ioctl!(readwrite get_lineinfo with 0xB4, 0x02; gpioline_info);
-ioctl!(readwrite get_linehandle with 0xB4, 0x03; gpiohandle_request);
-ioctl!(readwrite get_line_values with 0xB4, 0x08; gpiohandle_data);
-ioctl!(readwrite set_line_values with 0xB4, 0x09; gpiohandle_data);
+ioctl_read!(get_chipinfo, 0xB4, 0x01, gpiochip_info);
+ioctl_readwrite!(get_lineinfo, 0xB4, 0x02, gpioline_info);
+ioctl_readwrite!(get_linehandle, 0xB4, 0x03, gpiohandle_request);
+ioctl_readwrite!(get_line_values, 0xB4, 0x08, gpiohandle_data);
+ioctl_readwrite!(set_line_values, 0xB4, 0x09, gpiohandle_data);
 
-struct GPIOChip {
+pub struct GPIOChip {
   file: std::fs::File,
   name: String,
   label: String,
@@ -84,13 +83,13 @@ struct GPIOHandle {
 impl GPIOChip {
   fn new (path: &std::path::Path) -> std::io::Result<GPIOChip> {
     let file = try!(std::fs::File::open(path));
-    let (name, label, lines) = try!(GpioChip::chipinfo(file.as_raw_fd()));
-    Ok(GpioChip {file: file, name: name, label: label, lines: lines})
+    let (name, label, lines) = try!(GPIOChip::info(file.as_raw_fd()));
+    Ok(GPIOChip {file: file, name: name, label: label, lines: lines})
   }
 
   fn info (fd: RawFd) -> std::io::Result<(String, String, u32)> {
     let mut data = gpiochip_info { name: [0; 32], label: [0; 32], lines: 0 };
-    get_chipinfo(fd, &mut data);
+    unsafe { get_chipinfo(fd, &mut data); }
 
     let name = unsafe {std::ffi::CStr::from_ptr(data.name.as_ptr())}
       .to_string_lossy()
@@ -100,7 +99,7 @@ impl GPIOChip {
       .to_string_lossy()
       .into_owned();
 
-    Ok(name, label, data.lines)
+    Ok((name, label, data.lines))
   }
 
   fn request (&self, consumer: &str, flags: u32, gpio: u32, default: u8) -> std::io::Result<GPIOHandle> {
@@ -111,10 +110,10 @@ impl GPIOChip {
       consumer_label: [0; 32],
       lines: 0,
       fd: 0
-    }
+    };
 
     request.lineoffsets[0] = gpio;
-    request.flags = u32;
+    request.flags = flags;
     request.default_values[0] = default;
     request.lines = 1;
 
@@ -123,9 +122,9 @@ impl GPIOChip {
       request.consumer_label[i] = consumer.as_bytes()[i] as std::os::raw::c_char;
     }
 
-    get_linehandle(self.file.as_raw_fd(), &mut request);
+    unsafe { get_linehandle(self.file.as_raw_fd(), &mut request); }
     
-    Ok(GpioHandle {
+    Ok(GPIOHandle {
       file: unsafe {std::fs::File::from_raw_fd(request.fd)},
       consumer: consumer.to_string(),
       flags: flags,
@@ -137,32 +136,38 @@ impl GPIOChip {
 impl GPIOHandle {
   fn get (&self) -> std::io::Result<u8> {
     let mut data = gpiohandle_data { values: [0; 64] };
-    get_line_values(self.file.as_raw_fd(), &mut data);
+    unsafe { get_line_values(self.file.as_raw_fd(), &mut data); }
     Ok(data.values[0])
   }
 
   fn set (&self, state: u8) -> std::io::Result<()> {
     let mut data = gpiohandle_data { values: [0; 64] };
-    data.values[0] = value;
-    set_line_values(self.file.as_raw_fd(), &mut data);
+    data.values[0] = state;
+    unsafe { set_line_values(self.file.as_raw_fd(), &mut data).unwrap(); }
     Ok(())
   }
 }
 
 #[no_mangle]
-pub extern fn construct_gpiochip () -> *mut  GPIOChip {}
+pub extern fn construct_gpiochip (p: *const libc::c_char) -> *mut GPIOChip {
+  let c_str = unsafe { std::ffi::CStr::from_ptr(p) };
+  let r_str = c_str.to_str().unwrap();
+  let path = std::path::Path::new(r_str);
 
-#[no_mangle]
-pub extern fn destruct_gpiochip (*mut GPIOChip) {}
+  unsafe { std::mem::transmute(Box::new(GPIOChip::new(&path))) }
+}
 
-#[no_mangle]
-pub extern fn construct_gpioline () -> *mut GPIOHandle {}
+// #[no_mangle]
+// pub extern fn destruct_gpiochip (*mut GPIOChip) {}
 
-#[no_mangle]
-pub extern fn destruct_gpioline (*mut GPIOHandle) {}
+// #[no_mangle]
+// pub extern fn construct_gpioline () -> *mut GPIOHandle {}
 
-#[no_mangle]
-pub extern fn gpioline_get (*mut GPIOHandle) -> bool {}
+// #[no_mangle]
+// pub extern fn destruct_gpioline (*mut GPIOHandle) {}
 
-#[no_mangle]
-pub extern fn gpioline_set (*mut GPIOHandle, bool) {}
+// #[no_mangle]
+// pub extern fn gpioline_get (*mut GPIOHandle) -> bool {}
+
+// #[no_mangle]
+// pub extern fn gpioline_set (*mut GPIOHandle, bool) {}
