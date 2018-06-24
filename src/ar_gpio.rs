31c9c0612455e94684b6e759eb3f49032b7547fe
Shmish <shmish90@gmail.com>
2018-06-23 23:30:18 -0400

    restructure

--- src/ar_gpio.rs
+++ src/ar_gpio.rs
@@ -10,11 +10,27 @@
 extern crate nix;
 extern crate libc;
 
+mod RequestFlags {
+  const INPUT: u32 =       0x01 << 0;
+  const OUTPUT: u32 =      0x01 << 1;
+  const ACTIVE_LOW: u32 =  0x01 << 2;
+  const OPEN_DRAIN: u32 =  0x01 << 3;
+  const OPEN_SOURCE: u32 = 0x01 << 4;
+}
+
+mod LineFlags {
+  const KERNEL: u32 =      0x01 << 0;
+  const IS_OUT: u32 =      0x01 << 1;
+  const ACTIVE_LOW: u32 =  0x01 << 2;
+  const OPEN_DRAIN: u32 =  0x01 << 3;
+  const OPEN_SOURCE: u32 = 0x01 << 4;
+}
+
 #[repr(C)]
 struct gpiochip_info {
   name: [libc::c_char; 32],
   label: [libc::c_char; 32],
-  lines: libc::uint32_t
+  lines: libc::uint32_t,
 }
 
 #[repr(C)]
@@ -22,7 +38,7 @@ struct gpioline_info {
   line_offset: libc::uint32_t,
   flags: libc::uint32_t,
   name: [libc::c_char; 32],
-  consumer: [libc::c_char; 32]
+  consumer: [libc::c_char; 32],
 }
 
 #[repr(C)]
@@ -46,22 +62,6 @@ ioctl!(readwrite get_linehandle with 0xB4, 0x03; gpiohandle_request);
 ioctl!(readwrite get_line_values with 0xB4, 0x08; gpiohandle_data);
 ioctl!(readwrite set_line_values with 0xB4, 0x09; gpiohandle_data);
 
-mod RequestFlags {
-  const INPUT: uint32_t =       0x01 << 0;
-  const OUTPUT: uint32_t =      0x01 << 1;
-  const ACTIVE_LOW: uint32_t =  0x01 << 2;
-  const OPEN_DRAIN: uint32_t =  0x01 << 3;
-  const OPEN_SOURCE: uint32_t = 0x01 << 4;
-}
-
-mod LineFlags {
-  const KERNEL: uint32_t =      0x01 << 0;
-  const IS_OUT: uint32_t =      0x01 << 1;
-  const ACTIVE_LOW: uint32_t =  0x01 << 2;
-  const OPEN_DRAIN: uint32_t =  0x01 << 3;
-  const OPEN_SOURCE: uint32_t = 0x01 << 4;
-}
-
 struct GPIOChip {
   file: std::fs::File,
   name: String,
@@ -74,26 +74,4 @@ struct GPIOHandle {
   gpio: u32,
   consumer: String,
   flags: uint32_t
-}
-
-impl GPIOChip {
-  fn new (path: &std::path::Path) -> std::io::Result<GPIOChip> {
-    
-  }
-
-  fn info (fd: unix::io::RawFd) -> std::io::Result(String, String, u32) {
-    let mut info = gpiochip_info {
-      name: [0; 32],
-      label: [0; 32],
-      lines: 0
-    };
-
-    get_chipinfo(fd, &mut info);
-
-    let name = 
-  }
-
-  fn request () {
-
-  }
 }
