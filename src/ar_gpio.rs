124bff6eacb3fc143611dda5fe529e6678566317
shmishtopher <shmish90@gmail.com>
2018-06-23 20:32:27 -0400

    added ioctl

--- src/ar_gpio.rs
+++ src/ar_gpio.rs
@@ -1,34 +1,78 @@
+/**
+ * Copyright (C) 2018, Shmish <shmish90@gmail.com>
+ *
+ * This code is licensed under the MIT licence
+ * found in the LICENCE file in the root directory
+ * of this source tree.
+ */
+
 #[macro_use]
 extern crate nix;
 extern crate libc;
 
+use libc::c_char;
+use libc::uint32_t;
+use std::fs::File;
+use std::os::unix;
+
 #[repr(C)]
 struct gpiochip_info {
-  name: [libc::c_char; 32],
-  label: [libc::c_char; 32],
-  lines: libc::uint32_t
+  name: [c_char; 32],
+  label: [c_char; 32],
+  lines: uint32_t
 }
 
 #[repr(C)]
 struct gpioline_info {
-  line_offset: libc::uint32_t,
-  flags: libc::uint32_t,
-  name: [libc::c_char; 32],
-  consumer: [libc::c_char; 32]
+  line_offset: uint32_t,
+  flags: uint32_t,
+  name: [c_char; 32],
+  consumer: [c_char; 32]
 }
 
 mod RequestFlags {
-  const INPUT: libc::uint32_t =       0x01 << 0;
-  const OUTPUT: libc::uint32_t =      0x01 << 1;
-  const ACTIVE_LOW: libc::uint32_t =  0x01 << 2;
-  const OPEN_DRAIN: libc::uint32_t =  0x01 << 3;
-  const OPEN_SOURCE: libc::uint32_t = 0x01 << 4;
+  const INPUT: uint32_t =       0x01 << 0;
+  const OUTPUT: uint32_t =      0x01 << 1;
+  const ACTIVE_LOW: uint32_t =  0x01 << 2;
+  const OPEN_DRAIN: uint32_t =  0x01 << 3;
+  const OPEN_SOURCE: uint32_t = 0x01 << 4;
 }
 
 mod LineFlags {
-  const KERNEL: libc::uint32_t =      0x01 << 0;
-  const IS_OUT: libc::uint32_t =      0x01 << 1;
-  const ACTIVE_LOW: libc::uint32_t =  0x01 << 2;
-  const OPEN_DRAIN: libc::uint32_t =  0x01 << 3;
-  const OPEN_SOURCE: libc::uint32_t = 0x01 << 4;
+  const KERNEL: uint32_t =      0x01 << 0;
+  const IS_OUT: uint32_t =      0x01 << 1;
+  const ACTIVE_LOW: uint32_t =  0x01 << 2;
+  const OPEN_DRAIN: uint32_t =  0x01 << 3;
+  const OPEN_SOURCE: uint32_t = 0x01 << 4;
+}
+
+ioctl!(read get_chipinfo with 0xB4, 0x01; gpiochip_info);
+ioctl!(readwrite get_lineinfo with 0xB4, 0x01; gpioline_info);
+
+struct GPIOChip {
+  file: File,
+  name: String,
+  label: String,
+  lines: u32
+}
+
+struct GPIOHandle {
+  file: File,
+  gpio: u32,
+  consumer: String,
+  flags: uint32_t
+}
+
+impl GPIOChip {
+  fn new (path: &std::path::Path) -> std::io::Result<GPIOChip> {
+    
+  }
+
+  fn info (fd: unix::io::RawFd) -> std::io::Result(String, String, u32) {
+
+  }
+
+  fn request () {
+
+  }
 }
