628420999442bd23c76c39e66f9bec356e57a79f
Shmish <shmish90@gmail.com>
2018-06-25 13:47:39 -0400

    added gpiohandle

--- src/ar_gpio.rs
+++ src/ar_gpio.rs
@@ -47,15 +47,15 @@ ioctl_readwrite!(get_line_values, 0xB4, 0x08, gpiohandle_data);
 ioctl_readwrite!(set_line_values, 0xB4, 0x09, gpiohandle_data);
 
 #[no_mangle]
-pub extern fn gpiochip_info (s: *mut i8) -> *mut gpiochip_info {
-  let c_str = unsafe { std::ffi::CStr::from_ptr(s) };
-  let r_str = c_str.to_str().unwrap();
-
+pub extern fn gpiochip_info (fd: i32) -> *mut gpiochip_info {
   let mut info = gpiochip_info {
     name: [0; 32],
     label: [0; 32],
     lines: 0
   };
+
+  get_chipinfo(fd, &mut info);
+  unsafe { std::mem::transmute(Box::new(info)) }
 }
 
 #[no_mangle]
@@ -100,4 +100,24 @@ pub extern fn gpioline_info_name (ptr: *mut gpioline_info) -> [i8; 32] {
 pub extern fn gpioline_info_consumer (ptr: *mut gpioline_info) -> [i8; 32] {
   let _gpioline_info = unsafe { *ptr };
   _gpioline_info.consumer
+}
+
+
+
+#[no_mangle]
+pub extern fn gpiohandle_lineoffsets (ptr: *mut gpiohandle_data) -> [u32; 64] {
+  let _gpiohandle = unsafe { *ptr };
+  _gpiohandle.lineoffsets
+}
+
+#[no_mangle]
+pub extern fn gpiohandle_flags (ptr: *mut gpiohandle_data) -> u32 {
+  let _gpiohandle = unsafe { *ptr };
+  _gpiohandle.flags
+}
+
+#[no_mangle]
+pub extern fn gpiohandle_fd (ptr: *mut gpiohandle_data) -> i32 {
+  let _gpiohandle = unsafe { *ptr };
+  _gpiohandle.fd
 }
