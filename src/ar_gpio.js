b0f7a599bbf2910d3c31a88a87fa1fdf8f3926c0
Shmish <shmish90@gmail.com>
2018-06-26 13:15:24 -0400

    added GPIOChip and GPIOLine

--- src/ar_gpio.js
+++ src/ar_gpio.js
@@ -1,6 +1,48 @@
 const { Library } = require('fastcall')
 
-const lib = new Library(`${__dirname}/ar_gpio.so`)
-  .function('int32 gpiohandle_request (int32, int32, uint32)')
-  .function('void gpiohandle_set (int32, bool)')
-  .function('bool gpiohandle_get (int32)')
+const ffi = new Library(`${__dirname}/ar_gpio.so`)
+  .function('pointer gpiochip_create (int32)')
+  .function('string gpiochip_name (pointer)')
+  .function('string gpiochip_label (pointer)')
+  .function('uint32 gpiochip_lines (pointer)')
+  .function('void gpiochip_destroy (pointer)')
+
+  .function('pointer gpioline_create (int32)')
+  .function('uint32 gpioline_offset (pointer)')
+  .function('uint32 gpioline_flags (pointer)')
+  .function('string gpioline_name (pointer)')
+  .function('string gpioline_consumer (pointer)')
+  .function('void gpioline_destroy (pointer)')
+
+
+class GPIOChipInfo {
+  constructor (file) {
+    const $ptr = ffi.interface.gpiochip_create(file)
+
+    this.name = ffi.interface.gpiochip_name($ptr)
+    this.label = ffi.interface.gpiochip_label($ptr)
+    this.lines = ffi.interface.gpiochip_lines($ptr)
+
+    ffi.interface.gpiochip_destroy($ptr)
+    Object.freeze(this)
+  }
+}
+
+
+class GPIOLineInfo {
+  constructor (file) {
+    const $ptr = ffi.interface.gpioline_create(file)
+
+    this.offset = ffi.interface.gpioline_offset($ptr)
+    this.flags = ffi.interface.gpioline_flags($ptr)
+    this.name = ffi.interface.gpioline_name($ptr)
+    this.consumer = ffi.interface.gpioline_consumer($ptr)
+
+    ffi.interface.gpioline_destroy($ptr)
+    Object.freeze(this)
+  }
+}
+
+
+module.exports.GPIOChipInfo = GPIOChipInfo
+module.exports.GPIOLineInfo = GPIOLineInfo
--- src/ar_gpio.rs
+++ src/ar_gpio.rs
@@ -10,11 +10,26 @@
 extern crate nix;
 
 #[repr(C)]
+pub struct gpiochip_info {
+  pub name: [u8; 32],
+  pub label: [u8; 32],
+  pub lines: u32
+}
+
+#[repr(C)]
+pub struct gpioline_info {
+  pub line_offset: u32,
+  pub flags: u32,
+  pub name: [u8; 32],
+  pub consumer: [u8; 32]
+}
+
+#[repr(C)]
 pub struct gpiohandle_request {
   pub lineoffsets: [u32; 64],
   pub flags: u32,
   pub default_values: [u8; 64],
-  pub consumer_label: [i8; 64],
+  pub consumer_label: [u8, 32],
   pub lines: u32,
   pub fd: i32
 }
@@ -24,40 +39,80 @@ pub struct gpiohandle_data {
   pub values: [u8; 64]
 }
 
-ioctl_readwrite!(get_linehandle, 0xB4, 0x03, gpiohandle_request);
+ioctl_read!(get_chip_info, 0xB4, 0x01, gpiochip_info);
+ioctl_readwrite!(get_line_info, 0xB4, 0x02, gpioline_info);
+ioctl_readwrite!(get_line_handle, 0xB4, 0x02, gpiohandle_request);
 ioctl_readwrite!(get_line_values, 0xB4, 0x08, gpiohandle_data);
 ioctl_readwrite!(set_line_values, 0xB4, 0x09, gpiohandle_data);
 
-#[no_mangle]
-pub extern fn gpiohandle_request (fd: i32, line: i32, flags: u32) -> i32 {
-  let mut request = gpiohandle_request {
-    lineoffsets: [0; 64],
-    flags: 0;
-    default_values: [0; 64],
-    consumer_label: [0; 64],
-    lines: 0;
-    fd: 0
+
+pub extern fn gpiochip_create (fd: i32) -> *mut gpiochip_info {
+  let mut info = gpiochip_info {
+    name: [0; 32],
+    label: [0; 32],
+    lines: 0
   };
 
-  request.lineoffsets[0] = line;
-  request.flags = flags;
-
   unsafe {
-    get_linehandle(fd, &mut request);
-    request.fd
+    get_chip_info(fd, &mut info);
+    std::mem::transmute(Box::new(info))
   }
 }
 
-#[no_mangle]
-pub extern fn gpiohandle_set (fd: i32, value: bool) {
-  let mut data = gpiohandle_data { values: [0; 64] };
-  data.values[0] = if (value) { 1 } else { 0 };
-  unsafe { set_line_values(fd, &mut data); }
+pub extern fn gpiochip_name (ptr: *mut gpiochip_info) -> [u8; 32] {
+  let _gpiochip = unsafe { *ptr };
+  _gpiochip.name
 }
 
-#[no_mangle]
-pub extern fn gpiohandle_get (fd: i32) -> bool {
-  let mut data = gpiohandle_data { values: [0; 64] };
-  unsafe { get_line_values(fd, &mut data); }
-  if (data.values[0] == 0) { false } else { true }
+pub extern fn gpiochip_label (ptr: *mut gpiochip_info) -> [u8; 32] {
+  let _gpiochip = unsafe { *ptr };
+  _gpiochip.label
+}
+
+pub extern fn gpiochip_lines (ptr: *mut gpiochip_info) -> u32 {
+  let _gpiochip = unsafe { *ptr };
+  _gpiochip.lines
+}
+
+pub extern fn gpiochip_destroy (ptr: *mut gpiochip_info) {
+  let _gpiochip: Box<gpiochip_info> = unsafe { std::mem::transmute(ptr) };
+}
+
+
+pub extern fn gpioline_create (fd: i32) -> *mut gpioline_info {
+  let mut info = gpioline_info {
+    line_offset: 0,
+    flags: 0,
+    name: [0; 32],
+    consumer: [0; 32]
+  }
+
+  unsafe {
+    get_line_info(fd, &mut info);
+    std::meme::transmute(Box::new(info))
+  }
+}
+
+pub extern fn gpioline_offset (ptr: *mut gpioline_info) -> u32 {
+  let _gpioline = unsafe { *ptr };
+  _gpioline.line_offset
+}
+
+pub extern fn gpioline_flags (ptr: *mut gpioline_info) -> u32 {
+  let _gpioline = unsafe { *ptr };
+  _gpioline.flags
+}
+
+pub extern fn gpioline_name (ptr: *mut gpioline_info) -> [u8; 32] {
+  let _gpioline = unsafe { *ptr };
+  _gpioline.name
+}
+
+pub extern fn gpioline_consumer (ptr: *mut gpioline_info) -> [u8; 32] {
+  let _gpioline = unsafe { *ptr };
+  _gpioline.consumer
+}
+
+pub extern fn gpioline_destroy (ptr: *mut gpioline_info) {
+  let _gpioline: Box<gpioline_info> = unsafe { std::mem::transmute(ptr) };
 }
